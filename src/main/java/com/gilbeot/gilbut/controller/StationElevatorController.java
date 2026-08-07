22da3a399818c2fb51dee8d5faf8236fcb48038a
방가연 <gayeonb55@gmail.com>
2026-08-07 19:53:09 +0900

    feat: 경로 주변 역 승강기 조회 API 추가

--- src/main/java/com/gilbeot/gilbut/controller/StationElevatorController.java
+++ src/main/java/com/gilbeot/gilbut/controller/StationElevatorController.java
@@ -1,14 +1,20 @@
 package com.gilbeot.gilbut.controller;
 
+import com.gilbeot.gilbut.dto.station.request.AlongRouteStationElevatorRequest;
 import com.gilbeot.gilbut.dto.station.request.NearbyStationElevatorRequest;
+import com.gilbeot.gilbut.dto.station.response.AlongRouteStationElevatorResponse;
 import com.gilbeot.gilbut.dto.station.response.NearbyStationElevatorResponse;
 import com.gilbeot.gilbut.global.common.api.ApiResponse;
 import com.gilbeot.gilbut.global.common.code.SuccessCode;
+import com.gilbeot.gilbut.service.station.StationElevatorAlongRouteService;
 import com.gilbeot.gilbut.service.station.StationElevatorNearbyService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +25,8 @@ public class StationElevatorController {
 
     private final StationElevatorNearbyService
             stationElevatorNearbyService;
+    private final StationElevatorAlongRouteService
+            stationElevatorAlongRouteService;
 
     @GetMapping("/nearby")
     public ResponseEntity<ApiResponse<NearbyStationElevatorResponse>>
@@ -27,6 +35,23 @@ public class StationElevatorController {
     ) {
         NearbyStationElevatorResponse response =
                 stationElevatorNearbyService.findNearby(request);
+
+        return ApiResponse.success(
+                SuccessCode._OK,
+                response
+        );
+    }
+
+    @PostMapping("/along-route")
+    public ResponseEntity<ApiResponse<AlongRouteStationElevatorResponse>>
+    getAlongRouteStationElevators(
+            @Valid @RequestBody
+            AlongRouteStationElevatorRequest request
+    ) {
+        AlongRouteStationElevatorResponse response =
+                stationElevatorAlongRouteService.findAlongRoute(
+                        request
+                );
 
         return ApiResponse.success(
                 SuccessCode._OK,
